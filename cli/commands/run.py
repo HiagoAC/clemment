8864ce07f174b99558754019cae795158ddf780e
HiagoAC <hiago.uft@gmail.com>
2025-02-17 15:23:27 +0900

    Add run cli command for analyzing source code files and outputting suggestions

---
+++ cli/commands/run.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+
+from core.file_processing import analyse_comments_in_path
+
+
+def run(args: argparse.Namespace) -> None:
+    """
+    Analyse source code files in the given paths and output suggestions.
+    """
+    analysis_list = []
+    total_prompt_tokens = 0
+    total_completion_tokens = 0
+    if args.paths:
+        paths = args.paths
+    else:
+        paths = [os.getcwd()]
+    for path in paths:
+        if not os.path.exists(path):
+            raise ValueError(f"Invalid path: {path}")
+        responses = analyse_comments_in_path(path)
+        for res in responses:
+            analysis_list.append((res["file_path"], res["suggestions"]))
+            total_prompt_tokens += res["prompt_tokens"]
+            total_completion_tokens += res["completion_tokens"]
+
+    # Output analysis results
+    print(f"Total prompt tokens: {total_prompt_tokens}")
+    print(f"Total completion tokens: {total_completion_tokens}")
+    print("Suggestions:")
+    for path, suggestions in analysis_list:
+        print(f"File: {path}")
+        for line_number, suggestion in suggestions:
+            print(f"Line {line_number}: {suggestion}")
--- tests/test_cli/test_commands/test_run.py
+++ tests/test_cli/test_commands/test_run.py
@@ -1,14 +1,17 @@
+from argparse import Namespace
 from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
 
-from commands.run import run
+from cli import run
 
 
 class TestRun(TestCase):
-    @patch("run.analyse_comments_in_path")
+    @patch("os.path.exists", return_value=True)
+    @patch("cli.commands.run.analyse_comments_in_path")
     @patch("sys.stdout", new_callable=StringIO)
-    def test_run_with_valid_path(self, mock_stdout, mock_analyse_comments_in_path):
+    def test_run_with_valid_path(
+            self, mock_stdout, mock_analyse_comments_in_path, _):
         """Test run() with one valid path."""
         mock_analyse_comments_in_path.return_value = [
             {
@@ -18,7 +21,8 @@ class TestRun(TestCase):
                 "completion_tokens": 3,
             }
         ]
-        run({"paths": ["test.py"]})
+        args = Namespace(paths=["test.py"])
+        run(args)
         expected_output = (
             "Total prompt tokens: 2\n"
             "Total completion tokens: 3\n"
@@ -28,3 +32,48 @@ class TestRun(TestCase):
             "Line 2: Suggestion 2\n"
         )
         self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch("os.path.exists", return_value=True)
+    @patch("cli.commands.run.analyse_comments_in_path")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_run_with_multiple_valid_paths(
+            self, mock_stdout, mock_analyse_comments_in_path, _):
+        """Test run() with multiple valid paths."""
+        file_path = "a_test_file.py"
+        suggestions = [(1, "Suggestion 1"), (2, "Suggestion 2")]
+        prompt_tokens = 2
+        completion_tokens = 3
+        mock_analyse_comments_in_path.return_value = [
+            {
+                "file_path": file_path,
+                "suggestions": suggestions,
+                "prompt_tokens": prompt_tokens,
+                "completion_tokens": completion_tokens,
+            }
+        ]
+        args = Namespace(paths=["test.py", "test2.py"])
+        run(args)
+        expected_output = (
+            f"Total prompt tokens: {prompt_tokens*2}\n"
+            f"Total completion tokens: {completion_tokens*2}\n"
+            "Suggestions:\n"
+            f"File: {file_path}\n"
+            f"Line {suggestions[0][0]}: {suggestions[0][1]}\n"
+            f"Line {suggestions[1][0]}: {suggestions[1][1]}\n"
+            f"File: {file_path}\n"
+            f"Line {suggestions[0][0]}: {suggestions[0][1]}\n"
+            f"Line {suggestions[1][0]}: {suggestions[1][1]}\n"
+        )
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertTrue(True)
+
+    @patch("cli.commands.run.analyse_comments_in_path")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_run_with_invalid_path(
+            self, mock_stdout, mock_analyse_comments_in_path):
+        """Test run() with one invalid path."""
+        mock_analyse_comments_in_path.return_value = []
+        invalid_path = "invalid_path"
+        args = Namespace(paths=[invalid_path])
+        with self.assertRaises(ValueError):
+            run(args)
