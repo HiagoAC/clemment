bd5a5b47589776a5dbd5dafa20bb0e929e52b593
HiagoAC <hiago.uft@gmail.com>
2025-02-24 18:11:21 +0900

    Refactor test_file_processing.py to improve mocking

--- tests/test_core/test_file_processing.py
+++ tests/test_core/test_file_processing.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, Mock, patch, mock_open
+from unittest.mock import MagicMock, patch, mock_open
 
 from core.file_processing import process_file, analyse_comments_in_path
-from core.comment_analyser import CommentAnalyser
 
 
 class TestHelperFunctions(TestCase):
@@ -11,9 +10,14 @@ class TestHelperFunctions(TestCase):
         self.comment_analyser.analyse_comments.return_value = [(1, "A")]
         self.comment_analyser.total_prompt_tokens = 100
         self.comment_analyser.total_completion_tokens = 50
+        self.patcher_open = patch(
+            'builtins.open', new_callable=mock_open, read_data='source code')
+        self.patcher_open.start()
 
-    @patch('builtins.open', new_callable=mock_open, read_data='source code')
-    def test_process_file(self, mock_open):
+    def tearDown(self):
+        self.patcher_open.stop()
+
+    def test_process_file(self):
         response = process_file("path/to/file.py", self.comment_analyser)
         self.assertEqual(response, {
             "file_path": "path/to/file.py",
@@ -22,11 +26,10 @@ class TestHelperFunctions(TestCase):
             "completion_tokens": 50
         })
 
-    @patch('builtins.open', new_callable=mock_open, read_data='source code')
     @patch("os.path.isfile")
     @patch("os.walk")
     def test_analyse_comments_in_path_directory(
-        self, mock_walk, mock_isfile, mock_open):
+        self, mock_walk, mock_isfile):
         """
         Test that analyse_comments_in_path processes
         all files in a directory.
