f93210789575c61abe24f4ede08791603a3c2d55
HiagoAC <hiago.uft@gmail.com>
2025-03-02 17:37:17 +0900

    Add unit tests for TokenLimits class

---
+++ tests/test_core/test_token_limits.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from core.token_limits import TokenLimits
+
+
+class TestTokenLimits(TestCase):
+    def setUp(self):
+        self.token_limits = TokenLimits()
+    
+    def test_get_token_limit_with_known_model(self):
+        token_limit = self.token_limits.get_token_limit("o1")
+        self.assertEqual(
+            token_limit, self.token_limits._model_token_limits["o1"])
+
+    def test_get_token_limit_with_unknown_model(self):
+        token_limit = self.token_limits.get_token_limit("unknown-model")
+        self.assertEqual(
+            token_limit, min(self.token_limits._model_token_limits.values()))
