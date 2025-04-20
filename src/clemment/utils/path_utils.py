8f254038175fe16bff8daf4dcdf03114dea0cd0d
HiagoAC <hiago.uft@gmail.com>
2025-04-20 18:05:13 +0900

    Add discover_files utility function

--- src/clemment/utils/path_utils.py
+++ src/clemment/utils/path_utils.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from typing import List
 
@@ -13,3 +14,15 @@ def get_absolute_path(filename: str, subdirs: List[str] = list()) -> str:
     for subdir in subdirs:
         path = path / subdir
     return str(path / filename)
+
+
+def discover_files(path: Path) -> List[Path]:
+    """
+    Discover all files in a directory and its subdirectories.
+    """
+    files = []
+    for root, _, filenames in os.walk(path):
+        for filename in filenames:
+            file_path = os.path.join(root, filename)
+            files.append(file_path)
+    return files
--- tests/test_utils/test_path_utils.py
+++ tests/test_utils/test_path_utils.py
@@ -1,8 +1,9 @@
+import os
 from unittest import TestCase
 from unittest.mock import patch
 from pathlib import Path
 
-from src.clemment.utils.path_utils import get_absolute_path
+from src.clemment.utils.path_utils import get_absolute_path, discover_files
 
 
 class TestPathUtils(TestCase):
@@ -29,3 +30,28 @@ class TestPathUtils(TestCase):
 
         self.assertEqual(
             res, str(self.mock_project_root / subdir1 / subdir2 / filename))
+    
+    def test_discover_files_success(self):
+        """
+        Test discover_files returns a list of files in a directory and its
+        subdirectories.
+        """
+        path = Path('/project/root/subdir1')
+        subdirs = ['subdir2', 'subdir3']
+        file1 = 'file1'
+        file2 = 'file2'
+        file3 = 'file3'
+
+        with patch('os.walk') as mock_walk:
+            mock_walk.return_value = [
+                (path, subdirs, [file1, file2]),
+                (path / subdirs[0] / subdirs[1], [], [file3])
+            ]
+            result = discover_files(path)
+            expected = [
+                os.path.join(path, file1),
+                os.path.join(path, file2),
+                os.path.join(path, subdirs[0], subdirs[1], file3),
+            ]
+            print(expected)
+            self.assertEqual(result, expected)
